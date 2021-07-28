e4b506e320ab8b2099a94961491fee85f5ecede1
Camil Grabowski <camil.grabowski@uconn.edu>
2021-07-28 13:52:52 -0400

    done with player objects

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -34,3 +34,59 @@ test('gets inventory from player or returns false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+test('gets players health value', () => {
+    const player = new Player ('Camil');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if palyer is alive or not', () => {
+    const player = new Player ('Camil');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Camil');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
+
+  test("gets player's attack value", () => {
+    const player = new Player('Camil');
+    player.strength = 10;
+  
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+  });
+
+  test('adds a potion to the inventory', () => {
+      const player = new Player('Camil');
+      const oldCount = player.inventory.length; // what is the oldCount for? whats it represent?
+
+      player.addPotion(new Potion());
+
+      expect(player.inventory.length).toBeGreaterThan(oldCount);
+  });
+
+  test('uses a potion from inventory', () => {
+    const player = new Player('Camil');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
+  });
